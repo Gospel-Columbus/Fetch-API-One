8bfec72d2480200011c044b388f2bc9894f617bc
Gospel-Columbus <132568918+Gospel-Columbus@users.noreply.github.com>
2025-07-22 06:41:06 +0100

    commit these now

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
+//fetch each json item to the console.
 fetch('https://jsonplaceholder.typicode.com/posts')
-.then((response) => response.json())
-.then((json) => json.map((item) => {
-    console.log(item.userId);
-    // for accessing the json data title in the site
-     const p = document.createElement("p");
-     p.textContent = `Title: ${item.title}`;
-     document.getElementById("content").appendChild(p);
-}))
+  .then((response) => response.json())
+  .then((json) => json.map((data) => {
+     console.log(data.title);
+      // fetch each of the json items to the website.
+      const p = document.createElement("p");
+     p.textContent = `Id: ${data.id}, `;
+    document.getElementById("content").appendChild(p);
 
-
-
+  }));
+    
